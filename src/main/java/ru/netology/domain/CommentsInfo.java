8e2b3722d16e02f21a94f092747efdf926ad7a8c
Anna Zolotukhina <zolot.annet@gmail.com>
2020-06-13 16:21:35 +0300

    Change and add classes like vk posts

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -2,5 +2,48 @@ package ru.netology.domain;
 
 public class CommentsInfo {
     private long count;
-    private boolean canComment;
+    private int canPost;
+    private int groupsCanPost;
+    private boolean canClose;
+    private boolean canOpen;
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public int getCanPost() {
+        return canPost;
+    }
+
+    public void setCanPost(int canPost) {
+        this.canPost = canPost;
+    }
+
+    public int getGroupsCanPost() {
+        return groupsCanPost;
+    }
+
+    public void setGroupsCanPost(int groupsCanPost) {
+        this.groupsCanPost = groupsCanPost;
+    }
+
+    public boolean isCanClose() {
+        return canClose;
+    }
+
+    public void setCanClose(boolean canClose) {
+        this.canClose = canClose;
+    }
+
+    public boolean isCanOpen() {
+        return canOpen;
+    }
+
+    public void setCanOpen(boolean canOpen) {
+        this.canOpen = canOpen;
+    }
 }
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,37 +1,36 @@
 package ru.netology.domain;
 
 public class Post {
-    private long id;
-    private String dateOfPublication;
-    private String annotation;
-    private String contentUrl; //image, video, link and etc.
-    private long viewCounter;
-    private long countReports;
+    private int id;
+    private int toId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String text;
+    private String contentUrl; //image, video, link and etc. In Vk documentation this field named attachments.
+    private ViewInfo viewInfo;
+    private ReportsInfo reportsInfo;
     private CommentsInfo commentsInfo;
-    private long countLike;
+    private LikesInfo likesInfo;
+    private String postType;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
 
     public long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
-    }
-
-    public String getDateOfPublication() {
-        return dateOfPublication;
-    }
-
-    public void setDateOfPublication(String dateOfPublication) {
-        this.dateOfPublication = dateOfPublication;
-    }
-
-    public String getAnnotation() {
-        return annotation;
-    }
-
-    public void setAnnotation(String annotation) {
-        this.annotation = annotation;
     }
 
     public String getContentUrl() {
@@ -42,14 +41,6 @@ public class Post {
         this.contentUrl = contentUrl;
     }
 
-    public long getViewCounter() {
-        return viewCounter;
-    }
-
-    public void setViewCounter(long viewCounter) {
-        this.viewCounter = viewCounter;
-    }
-
     public CommentsInfo getCommentsInfo() {
         return commentsInfo;
     }
@@ -58,19 +49,163 @@ public class Post {
         this.commentsInfo = commentsInfo;
     }
 
-    public long getCountLike() {
-        return countLike;
+    public int getToId() {
+        return toId;
     }
 
-    public void setCountLike(long countLike) {
-        this.countLike = countLike;
+    public void setToId(int toId) {
+        this.toId = toId;
     }
 
-    public long getCountReports() {
-        return countReports;
+    public int getFromId() {
+        return fromId;
     }
 
-    public void setCountReports(long countReports) {
-        this.countReports = countReports;
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ViewInfo getViewInfo() {
+        return viewInfo;
+    }
+
+    public void setViewInfo(ViewInfo viewInfo) {
+        this.viewInfo = viewInfo;
+    }
+
+    public ReportsInfo getReportsInfo() {
+        return reportsInfo;
+    }
+
+    public void setReportsInfo(ReportsInfo reportsInfo) {
+        this.reportsInfo = reportsInfo;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(int canPin) {
+        this.canPin = canPin;
+    }
+
+    public int getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(int canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public int getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(int canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public int getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(int markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
     }
 }
